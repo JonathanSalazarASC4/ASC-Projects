037e655d222f4dd8f62633ff0d52364faf6b4e47
JonathanSalazarASC4 <Jonathan.salazar9000@gmail.com>
2017-07-13 09:30:55 -0400

    Organized Files for Week 0

---
+++ week0/pokemonfunction.js
@@ -0,0 +1,28 @@
+var letters = ["A" , "B" , "C", "D" ,"E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"];
+var wholeLetters;
+
+function randLetter(array){
+
+    wholeLetters = array[Math.floor(Math.random() *letters.length)];
+    return wholeLetters;
+
+}
+
+console.log(randLetter(letters));
+
+function randLetter(length){
+
+for (var i = 0; i < letters.length; i++ );{
+
+str = str + (randLetter(letters)){
+
+    return 
+
+}
+
+
+
+
+ 
+
+
